412103a572ca3202bce0031339027e46fb375c78
Tarun Batra <tarun.batra00@gmail.com>
2015-07-13 20:03:44 +0530

    stack ADT with push,pop and peek

---
+++ Stack/Main.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2015 tbking.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package Stack;
+
+/**
+ *
+ * @author tbking
+ */
+// this class only exists to show the usage of the Stackclass
+public class Main {
+    public static void main(String[] args) {
+        Stack stck=new Stack();
+        System.out.println(stck.toString());    // printing empty stack
+        stck.push(1);                           // pushing an int to stack
+        stck.push('c');                         // pushing a character to stack
+        stck.push("string");                    // pushing a string to stack
+        System.out.println(stck.toString());    // printing the stack
+        System.out.println(stck.peek());        // peeking from stack
+        System.out.println(stck.pop());         // popping from stack
+        System.out.println(stck.toString());    //printing the stack
+    }
+}
+/* output:
+null
+[ string | c | 1 ]
+string
+string
+[ c | 1 ]
+*/
---
+++ Stack/Stack.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2015 tbking.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package Stack;
+
+import LinkedList.LinkedList;
+
+/**
+ *
+ * @author tbking
+ */
+public class Stack {
+    private LinkedList list=null;
+    public Stack()
+    {
+        list=new LinkedList();
+    }
+    public <T> Stack(T data)
+    {
+        list=new LinkedList(data);
+    }
+    public <T> Stack(T[] data)
+    {
+        list=new LinkedList(data);
+    }
+    public <T> void push(T data)
+    {
+        list.insert(data,0);
+    }
+    public String pop()
+    {
+        String temp=list.getData(0);
+        list.delete(0);
+        return temp;        
+    }
+    public String peek()
+    {
+        return list.getData(0);     
+    }
+    public boolean isEmpty()
+    {
+        return list.getSize()==0;
+    }
+    @Override
+    public String toString()
+    {
+        if(this.isEmpty())
+            return null;
+        return list.toString().replace("->", "|");
+    }
+}
